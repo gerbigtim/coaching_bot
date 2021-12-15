fa4091450cfd1b146ee710db7da6b51bed403593
Tim Gerbig <gerbig.tim@gmail.com>
2021-12-15 12:43:42 +0100

    Change logging level to DEBUG

--- bot/logEnabler.py
+++ bot/logEnabler.py
@@ -2,9 +2,8 @@ import logging
 
 # Enable logging
 logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG
     # change logger config to print sent messages to terminal
 )
 
 logger = logging.getLogger(__name__)
-
